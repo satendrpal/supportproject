a0d1a3a777c37b21878bfb8612c0dcd640272a59
satendra <satendrap270@gmail.com>
2025-10-09 19:16:30 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,24 +5,18 @@ import UserMasterIndex from './Componets/UserMaster/index';
 import UserMasterCreate from './Componets/UserMaster/create'
 import Index from './Componets/index'
 import Dashboard from './Componets/Dashboard';
-<<<<<<< HEAD
-=======
+
 import Login from './login';
->>>>>>> f50791c480f05bead5a99f78e913b9282be82907
-
-
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Index />} />
         <Route path='/dashboard' element={<Dashboard/>}/>
-=======
+
       <Route path="/" element={<Login />} />
         <Route path="/index" element={<Index />} />
         <Route path="/dashboard" element={<Dashboard />} />
->>>>>>> f50791c480f05bead5a99f78e913b9282be82907
         <Route path="/user-master" element={<UserMasterIndex />} />
         <Route path="/user-master/create" element={<UserMasterCreate />} />
       </Routes>
